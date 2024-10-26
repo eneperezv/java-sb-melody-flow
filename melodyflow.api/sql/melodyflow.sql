365eede21a53a92d5ffbbe742a910821ddf218e7
eneperezv <eneperezv@gmail.com>
2024-10-25 20:30:53 -0500

    Se incluye archivo de respaldo de la base de datos
     Changes to be committed:
    	modified:   melodyflow.api/sql/melodyflow.sql

--- melodyflow.api/sql/melodyflow.sql
+++ melodyflow.api/sql/melodyflow.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-10-2024 a las 07:24:42
+-- Tiempo de generación: 26-10-2024 a las 03:30:20
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
